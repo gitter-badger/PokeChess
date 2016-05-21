f49c789b077076eb1729e0714b3d674b636ee8b4
mpatc <matthewpclancy@gmail.com>
2016-05-21 15:42:16 -0700

    add default and chess logic

---
+++ public/default.js
@@ -0,0 +1,37 @@
+var board;
+var game;
+
+window.onload = function () {
+    initGame();
+};
+
+var initGame = function() {
+   var cfg = {
+       draggable: true,
+       position: 'start',
+       onDrop: handleMove,
+   };
+
+   board = new ChessBoard('gameBoard', cfg);
+   game = new Chess();
+};
+
+var handleMove = function(source, target ) {
+    var move = game.move({from: source, to: target});
+    if (move === null)  return 'snapback';
+    window.setTimeout(makeRandomMove, 500);
+};
+var socket = io();
+
+var makeRandomMove = function() {
+  var possibleMoves = game.moves();
+
+  // exit if the game is over
+  if (game.game_over() === true ||
+    game.in_draw() === true ||
+    possibleMoves.length === 0) return;
+
+  var randomIndex = Math.floor(Math.random() * possibleMoves.length);
+  game.move(possibleMoves[randomIndex]);
+  board.position(game.fen());
+};
